05003b05e408373b568a2f4abb27b97b20df10f3
SCL1334 <violetlan1122@gmail.com>
2021-12-19 12:05:22 +0800

    test10 db

--- db_operator/read_from_db.py
+++ db_operator/read_from_db.py
@@ -16,3 +16,37 @@ def read_db(table):
     finally:
         cur.close()
         return results
+
+
+def read_city():
+    postgres_manager = PostgresBaseManager()
+    cur = postgres_manager.conn.cursor()
+    try:
+        cur.execute(
+            "SELECT DISTINCT cityID, cityName FROM City_Town;")
+        # Retrieve all rows from the PostgreSQL table
+        results = cur.fetchall()
+        postgres_manager.conn.commit()
+    except Exception as e:
+        print("Read failed.")
+        print(e)
+    finally:
+        cur.close()
+        return results
+
+
+def read_town_by_city(city):
+    postgres_manager = PostgresBaseManager()
+    cur = postgres_manager.conn.cursor()
+    try:
+        cur.execute(
+            f"SELECT townName FROM City_Town WHERE cityName='{city}';")
+        # Retrieve all rows from the PostgreSQL table
+        results = cur.fetchall()
+        postgres_manager.conn.commit()
+    except Exception as e:
+        print("Read failed.")
+        print(e)
+    finally:
+        cur.close()
+        return results
