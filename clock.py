066590d78878d8a639f79db4789237c1ea751309
SCL1334 <violetlan1122@gmail.com>
2021-12-21 21:14:21 +0800

    real schedule

--- clock.py
+++ clock.py
@@ -1,23 +1,16 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-# from datetime import datetime
-# import sys
-# from db_operator.update_from_wra import update_rain_warning, update_water_warning, update_reservoir_warning
+from datetime import datetime
+from db_operator.update_from_wra import update_rain_warning, update_water_warning, update_reservoir_warning
 
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', id='save_warn', minutes=1)
-def test():
-    print('This job if for test.')
-
-
-# @sched.scheduled_job('interval', id='save_warn', minutes=10)
-# def save_warn_from_wra():
-#     print('task executed at ' + str(datetime.now()))
-#     sys.stdout.flush()
-#     update_rain_warning()
-#     update_water_warning()
-#     update_reservoir_warning()
+@sched.scheduled_job('interval', id='save_warn', minutes=10)
+def save_warn_from_wra():
+    print('task executed at ' + str(datetime.now()))
+    update_rain_warning()
+    update_water_warning()
+    update_reservoir_warning()
 
 
 sched.start()
