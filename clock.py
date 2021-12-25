0892182fd2f6eae6bf85450f329774c93336bcd7
SCL1334 <violetlan1122@gmail.com>
2021-12-25 17:21:52 +0800

    stop test

--- clock.py
+++ clock.py
@@ -17,15 +17,15 @@ def save_warn_from_wra():
     update_reservoir_warning()
 
 
-@sched.scheduled_job('interval', id='send_warn', minutes=1)
-def send_mail():
-    with current_app.app_context():
-        usr_id = 2
-        sql = f"SELECT usrname, email from Usr WHERE id='{usr_id}';"
-        usr_detail = read_one(sql)
-        info = f"Hi {usr_detail['usrname']}"
-        usr_mail = usr_detail["email"]
-        send_warn(usr_mail, info)
+# @sched.scheduled_job('interval', id='send_warn', minutes=1)
+# def send_mail():
+#     with current_app.app_context():
+#         usr_id = 2
+#         sql = f"SELECT usrname, email from Usr WHERE id='{usr_id}';"
+#         usr_detail = read_one(sql)
+#         info = f"Hi {usr_detail['usrname']}"
+#         usr_mail = usr_detail["email"]
+#         send_warn(usr_mail, info)
 
 
 sched.start()
