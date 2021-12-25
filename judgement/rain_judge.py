6fb3532ec3b270d550e2cd496a525da5508e0fb9
sophiammm <m654631@hotmail.com>
2021-12-25 13:48:59 +0800

    judgement folder

---
+++ judgement/rain_judge.py
@@ -0,0 +1,95 @@
+from db_operator.base_manager import PostgresBaseManager
+from gps_address import in_range
+
+
+def operation(sql):
+    postgres_manager = PostgresBaseManager()
+    cur = postgres_manager.conn.cursor()
+    results = []
+    try:
+        cur.execute(sql)
+        # Retrieve all rows from the PostgreSQL table
+        results = cur.fetchall()
+        postgres_manager.conn.commit()
+    except Exception as e:
+        print("Read failed.")
+        print(e)
+    finally:
+        cur.close()
+        return results
+    
+
+def rain_judge_by_town(user_town_code):
+    read_rain_warn_sql = "SELECT * FROM Rain_Warning"
+    # staionNo, townCode, APItime, DBtime, warninglevel [1, 2]
+    datas = operation(read_rain_warn_sql)
+    result = []
+    warnings = []
+    for data in datas:
+        townCode = data[1]
+        level = data[4]
+        if user_town_code == townCode:
+            warnings.append(level)
+    
+    warnings.sort(reverse = True)
+    if warnings == []:
+        msg = {"warningLevel": 0}
+        result.append(msg)
+    else:
+        msg = {"warningLevel": warnings[0]}
+        result.append(msg)
+
+    return result
+    # 0: 沒有警戒; 1: 一級警戒; 2: 二級警戒
+
+
+def rain_judge_by_location(latitude, longitude):
+    read_rain_warn_sql = "SELECT * FROM Rain_Warning"
+    # staionNo, townCode, APItime, DBtime, warninglevel [1, 2]
+    datas = operation(read_rain_warn_sql)
+    # staionNo, latitude, longitude
+    read_rain_station_sql = "SELECT * FROM Rain_Station"
+    stations = operation(read_rain_station_sql)
+    near_stations = []
+    for station in stations:
+        user_location = (latitude, longitude)
+        station_location = (station[1], station[2])
+        is_close = in_range(user_location, station_location, 5)
+        stationno = station[0]
+        if is_close:
+            near_stations.append(stationno)
+
+
+    result = []
+    warnings = []
+    for i in range(len(near_stations)):
+        for j in range(len(datas)):
+            if near_stations[i] == datas[j][0]:
+                warnings.append(datas[j][4])
+    
+    warnings.sort(reverse = True)
+    if warnings == []:
+        msg = {"warningLevel": 0}
+        result.append(msg)
+    else:
+        msg = {"warningLevel": warnings[0]}
+        result.append(msg)
+
+    return result
+    # 0: 沒有警戒; 1: 一級警戒; 2: 二級警戒
+
+
+if __name__ == "__main__":
+    # no case
+    # user_town_code = "6300400"
+
+    # # fit 1 case
+    # user_town_code = "1001301"
+
+    # # fit 2 case
+    # user_town_code = "6500900"
+
+    # print(rain_judge_by_town(user_town_code))
+
+    print(rain_judge_by_location(24.815376, 121.726958))
+
---
+++ judgement/water_judge.py
@@ -0,0 +1,63 @@
+from db_operator.base_manager import PostgresBaseManager
+from db_operator.read_from_db import read_water_station
+from gps_address import in_range
+
+
+def operation(sql):
+    postgres_manager = PostgresBaseManager()
+    cur = postgres_manager.conn.cursor()
+    results = []
+    try:
+        cur.execute(sql)
+        # Retrieve all rows from the PostgreSQL table
+        results = cur.fetchall()
+        postgres_manager.conn.commit()
+    except Exception as e:
+        print("Read failed.")
+        print(e)
+    finally:
+        cur.close()
+        return results
+    
+
+def water_judge_by_town(user_town_code):
+    read_water_warn_sql = "SELECT stationNo, townCode, warningLevel FROM Water_Warning;"
+    datas = operation(read_water_warn_sql)
+    warnings = []
+    for data in datas:
+        stationNo = data[0]
+        townCode = data[1]
+        level = data[2]
+        if user_town_code == townCode:
+            warnings.append((stationNo, level))
+    final_warnings = {}
+    for warning in warnings:
+        station_basinName = read_water_station(warning[0])[0][0]
+        if final_warnings.__contains__(station_basinName): # 如果final_warnings字典裡包含此流域
+            if warning[1] < final_warnings[station_basinName]: # 再判斷其流域的警戒是否比字典裡的嚴重
+                final_warnings.update({station_basinName:warning[1]}) # 若更嚴重，就覆蓋字典裡的value
+        else: # final_warnings字典裡未包含此流域
+            final_warnings.update({station_basinName:warning[1]}) # 新增此流域至字典裡
+    return final_warnings # e.g. {'秀姑巒溪': 2, '淡水河': 1} 若無警戒，則return {}
+
+
+def water_judge_by_location(latitude, longitude):
+    user_location = (latitude, longitude)
+    read_water_warn_sql = "SELECT stationNo, warningLevel FROM Water_Warning;"
+    warnings = operation(read_water_warn_sql)
+    final_warnings = {}
+    for warning in warnings:
+        station_basinName = read_water_station(warning[0])[0][0]
+        station_latitude = read_water_station(warning[0])[0][1]
+        station_longitude = read_water_station(warning[0])[0][2]
+        station_location = (station_latitude, station_longitude)
+        if in_range(
+            (station_location), (user_location), 5
+        ) is True: # 篩選出距離user 5公里內 有warning的測站
+            if final_warnings.__contains__(station_basinName): # 如果final_warnings字典裡包含此流域
+                if warning[1] < final_warnings[station_basinName]: # 再判斷其流域的警戒是否比字典裡的嚴重
+                    final_warnings.update({station_basinName:warning[1]}) # 若更嚴重，就覆蓋字典裡的value
+            else: # final_warnings字典裡未包含此流域
+                final_warnings.update({station_basinName:warning[1]}) # 新增此流域至字典裡
+    return final_warnings # e.g. {'大甲溪': 1, '大安溪': 2} 若無警戒，則return {}
+
