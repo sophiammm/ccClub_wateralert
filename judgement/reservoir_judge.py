13f0831e8a559e56c3df1f8cadb9e269219dc175
SCL1334 <violetlan1122@gmail.com>
2021-12-25 20:56:22 +0800

    fix if no spill time

--- judgement/reservoir_judge.py
+++ judgement/reservoir_judge.py
@@ -1,8 +1,7 @@
+from db_operator.read_from_db import read
+from datetime import datetime
 import sys
 sys.path.append("../")
-
-from datetime import datetime
-from db_operator.read_from_db import read
 
 
 def reservoir_judge(user_town_code):
@@ -11,20 +10,33 @@ def reservoir_judge(user_town_code):
     datas = read(read_re_warn_sql)
     result = []
     for data in datas:
-        nextSpillTime = datetime.fromtimestamp(data[4])
-        cur_time = datetime.now()
-        delta_time = (nextSpillTime - cur_time).seconds
         status = data[5]
-        # check time first to reduce times to read DB
-        # in case if status doesn't fit time, all the cases in status "1" would be reported
-        if delta_time <= 108000 or status[0] == "1":
-            stationNo = data[0]
-            get_affect_sql = f"SELECT townCode FROM Reservoir_AffectedArea WHERE stationNo='{stationNo}'"
+        # has nextSpillTime
+        try:
+            nextSpillTime = datetime.fromtimestamp(data[4])
+            cur_time = datetime.now()
+            delta_time = (nextSpillTime - cur_time).seconds
+            # check time first to reduce times to read DB
+            # in case if status doesn't fit time, all the cases in status "1" would be reported
+            if delta_time <= 108000 or status[0] == "1":
+                stationNo = data[0]
+                get_affect_sql = f"SELECT townCode FROM Reservoir_AffectedArea WHERE stationNo='{stationNo}'"
+                affect_area = read(get_affect_sql)
+            nextSpillTime = nextSpillTime.strftime("%Y-%m-%d %H:%M")
+        # no nextSpillTime
+        except:
+            if status[0] == "1":
+                stationNo = data[0]
+                get_affect_sql = f"SELECT townCode FROM Reservoir_AffectedArea WHERE stationNo='{stationNo}'"
+                affect_area = read(get_affect_sql)
+            nextSpillTime = "目前無資料"
+        finally:
             # [(townCode1), (townCode2),...]
-            affect_area = read(get_affect_sql)
+            status = status.split(":")[1]
             for i in affect_area:
                 # if user in the affected area and nextSpillTime - cur_time <= 3hr(10800sec)
                 if user_town_code == i[0]:
+
                     get_reservoir_name_sql = f"SELECT stationName FROM Reservoir_Station WHERE stationNo='{stationNo}'"
                     # [(stationName)] => String
                     reservoir_name = read(get_reservoir_name_sql)[0][0]
@@ -32,14 +44,14 @@ def reservoir_judge(user_town_code):
                            "nextSpillTime": nextSpillTime, "status": status}
                     result.append(msg)
 
-    # [{'reservoir_name': string, 'nextSpillTime': datetime.datetime(object)), 'status': string}, ...]
+    # [{'reservoir_name': string, 'nextSpillTime': string, 'status': string}, ...]
     return result
 
 
 if __name__ == "__main__":
     # # fit 1 case
-    # user_town_code = "6700100"
+    user_town_code = "6700100"
 
     # fit 2 case
-    user_town_code = "6700600"
+    # user_town_code = "6700600"
     print(reservoir_judge(user_town_code))
