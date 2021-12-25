311f3a237f83fc66c5bcf335ef82d7a48558d085
sophiammm <m654631@hotmail.com>
2021-12-25 15:14:26 +0800

    revise judge

--- judgement/reservoir_judge.py
+++ judgement/reservoir_judge.py
@@ -1,32 +1,15 @@
+import sys
+sys.path.append("../")
+
 from datetime import datetime
 from db_operator.base_manager import PostgresBaseManager
-from db_operator.save_from_wra import save_fake_to_reservoir_warning
-
-
-def operation(sql):
-    postgres_manager = PostgresBaseManager()
-    cur = postgres_manager.conn.cursor()
-    results = []
-    try:
-        cur.execute(sql)
-        # Retrieve all rows from the PostgreSQL table
-        results = cur.fetchall()
-        postgres_manager.conn.commit()
-    except Exception as e:
-        print("Read failed.")
-        print(e)
-    finally:
-        cur.close()
-        return results
-
-# # 加入新測資
-# save_fake_to_reservoir_warning()
+from db_operator.read_from_db import read
 
 
 def reservoir_judge(user_town_code):
     read_re_warn_sql = "SELECT * FROM Reservoir_Warning"
     # staionNo, townCode, APItime, DBtime, nextSpillTime, status ['0: 預計放水', '1: 放水中', '-1: 未放水']
-    datas = operation(read_re_warn_sql)
+    datas = read(read_re_warn_sql)
     result = []
     for data in datas:
         nextSpillTime = datetime.fromtimestamp(data[4])
@@ -39,13 +22,13 @@ def reservoir_judge(user_town_code):
             stationNo = data[0]
             get_affect_sql = f"SELECT townCode FROM Reservoir_AffectedArea WHERE stationNo='{stationNo}'"
             # [(townCode1), (townCode2),...]
-            affect_area = operation(get_affect_sql)
+            affect_area = read(get_affect_sql)
             for i in affect_area:
                 # if user in the affected area and nextSpillTime - cur_time <= 3hr(10800sec)
                 if user_town_code == i[0]:
                     get_reservoir_name_sql = f"SELECT stationName FROM Reservoir_Station WHERE stationNo='{stationNo}'"
                     # [(stationName)] => String
-                    reservoir_name = operation(get_reservoir_name_sql)[0][0]
+                    reservoir_name = read(get_reservoir_name_sql)[0][0]
                     msg = {"reservoir_name": reservoir_name,
                            "nextSpillTime": nextSpillTime, "status": status}
                     result.append(msg)
